1e2279b16de439afab9757a6ad288bef6e410ee6
Markus Braun <a12345@MacBook-Air-von-Markus.local>
2026-08-16 19:45:30 +0200

    Force refresh photo asset

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const CONFIG = {
     "Pizza, Spiele und Kuscheldecke",
     "Überraschung ;)"
   ],
-  photoPath: "assets/photo.jpg"
+  photoPath: "assets/photo.jpg?v=20260816-2"
 };
 
 const STORAGE_KEY = "pixelDateChoice";
